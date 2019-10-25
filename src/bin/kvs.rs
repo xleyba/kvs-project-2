4a6891b12ee85d6df54bb6828f48ebe0babab89c
Javier L <135663+xleyba@users.noreply.github.com>
2019-10-25 12:46:05 +0200

    Added methods

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+use clap::{App, AppSettings, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .arg(Arg::with_name("KEY").help("A string key").required(true))
+                .arg(
+                    Arg::with_name("VALUE")
+                        .help("The string value of the key")
+                        .required(true),
+                ),
+        )
+        .get_matches();
 }
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+
+pub struct KvStore;
+
+impl KvStore {
+    
+    pub fn new() -> KvStore {
+        KvStore
+    }
+
+    pub fn get(&mut self, key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        
+    }
+
+    pub fn remove(&mut self, key: String) {
+        
+    }
+
+}
